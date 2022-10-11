57e21597910140c6f0d003dd6b4a38d94afaaf75
siri214 <choijs214@naver.com>
2022-10-11 15:41:28 +0900

    thirdB commit

---
+++ src/main/java/Wku/OSS/Repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package Wku.OSS.Repository;
+
+import Wku.OSS.Entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+
+
+}
