7663dd19b0a29780cb7b4fe5f88a88862e50d963
siri214 <choijs214@naver.com>
2022-10-11 15:36:19 +0900

    secondB commit

---
+++ src/main/java/Wku/OSS/Repository/UserInfoRepository.java
@@ -0,0 +1,10 @@
+package Wku.OSS.Repository;
+
+import Wku.OSS.Entity.UserInfo;
+import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+}
