dcb40e5258ec4c76ef0258c361df874df35c1073
siri214 <choijs214@naver.com>
2022-10-11 15:32:22 +0900

    second Branch commit

---
+++ src/main/java/Wku/OSS/Entity/UserInfo.java
@@ -0,0 +1,16 @@
+package Wku.OSS.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long uid;
+
+    private String title;
+}
